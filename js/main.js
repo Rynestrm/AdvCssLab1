c1fcaa497d72ae11323aa9c35a4e23759304422c
unknown <gibson.ivan@gmail.com>
2020-09-18 14:16:20 -0400

    added some CSS and JS

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// function to add a class
+function hideElement() {
+    let hidden = document.querySelector(".footerNote");
+    hidden.classList.add("hidden");
+}
+
+// runs on page load
+hideElement();
